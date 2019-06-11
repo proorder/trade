63857d27c9a05c0f968005ae063cbbac04e012af
Vyacheslav <supvlmail@gmail.com>
2019-06-11 15:40:05 +0500

    First commit

--- templates/scripts/main.js
+++ templates/scripts/main.js
@@ -1,1 +1,1 @@
-alert();
+console.log(csv);
---
+++ trade/views.py
@@ -0,0 +1,20 @@
+from django.views.generic import View
+from django.shortcuts import render
+import pandas as pd, json
+
+class MainView(View):
+    def getCSVJSON(self):
+        table = pd.read_csv('static/yndx.csv')
+        output = []
+        for index, row in table.iterrows():
+            output.append([
+                    row['<OPEN>'],
+                    row['<HIGH>'],
+                    row['<LOW>'],
+                    row['<CLOSE>'],
+                ])
+        return json.dumps(output)
+
+    def get(self, request, *args, **kwargs):
+        context = {'csv': self.getCSVJSON()}
+        return render(request, 'index.html', context=context)
